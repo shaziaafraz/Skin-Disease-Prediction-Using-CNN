04664ae40868bc0bf2082e7c0cf292a65d15ce36
Shazia Afraz <shaziaafraz23@gmail.com>
2025-10-26 19:40:11 +0530

    start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd /app
-export PYTHONPATH=/app:$PYTHONPATH
 export DJANGO_SETTINGS_MODULE=SkinDisease.settings
 export DEBUG=True
 export ALLOWED_HOSTS=*
-python manage.py runserver 0.0.0.0:8000
+PYTHONPATH=/app python manage.py runserver 0.0.0.0:8000
