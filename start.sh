199569f10dfb1766f98e7f352f00afef073ae71b
Shazia Afraz <shaziaafraz23@gmail.com>
2025-10-26 19:30:41 +0530

    start.sh
    Set environment variables for Django application.

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 cd /app
 export PYTHONPATH=/app:$PYTHONPATH
+export DJANGO_SETTINGS_MODULE=SkinDisease.settings
+export DEBUG=True
+export ALLOWED_HOSTS=*
 python manage.py runserver 0.0.0.0:8000
